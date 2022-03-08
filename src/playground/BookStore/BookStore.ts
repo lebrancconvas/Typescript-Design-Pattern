c145c0dd65907c636fc409cfe3c453a1d4f879c4
lebrancconvas <lebrancconvas@gmail.com>
2022-03-09 00:38:19 +0700

    🔨 Update MangaType.

--- src/playground/BookStore/BookStore.ts
+++ src/playground/BookStore/BookStore.ts
@@ -1,12 +1,15 @@
 enum MangaType {
-	Shounen,
-	Shoujo,
-	Seinen
+	Shounen = "Shounen",
+	Shoujo = "Shoujo",
+	Seinen = "Seinen",
+	Josei = "Josei",
+	Kodomomuke = "Kodomomuke"
 };
 
 interface IBookList {
 	BookID: Number;
 	BookName: String;
+	BookGenre: MangaType;
 	BookDescription: String;
 	BookPrice: Number;
 };
@@ -14,6 +17,7 @@ interface IBookList {
 export let Book01: IBookList = {
 	BookID: 1,
 	BookName: "The Guide to be the Dragon Master",
+	BookGenre: MangaType.Shounen,
 	BookDescription: "If you want to be the dragon master, Read it.",
 	BookPrice: 233
 };

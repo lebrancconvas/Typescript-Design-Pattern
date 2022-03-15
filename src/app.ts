f7e060df55c15d2ce42efa4c901995be46e25a06
lebrancconvas <lebrancconvas@gmail.com>
2022-03-16 01:33:13 +0700

    ✨ Moving.

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,37 @@ import {Book01} from './playground/BookStore/BookStore';
 import {Rectangle} from './playground/GraphicRenderer/GraphicRenderer';
 import {squid01} from './playground/SquidGame/SquidGame';
 
+
+// Squid Moving. 
+let moveSquid = (direction: String, step: number) => {
+	for(let i = 1; i <= step; i++) {
+		switch(direction) {
+			case "Right":
+				squid01.walkRight();
+				break;
+			case "Left":
+				squid01.walkLeft();
+				break;
+			case "Up":
+				squid01.walkUp();
+				break;
+			case "Down":
+				squid01.walkDown();
+				break;
+		}
+	}
+}
+
+
+// Squid Operation. 
 squid01.displayPoint();
-squid01.walkRight();
-squid01.walkRight(); 
+
+moveSquid("Right", 3);
+moveSquid("Down", 5);
+squid01.displayPoint(); 
+
+moveSquid("Down", 2);
 squid01.displayPoint();
+
+moveSquid("Up", 5);
+squid01.displayPoint(); 

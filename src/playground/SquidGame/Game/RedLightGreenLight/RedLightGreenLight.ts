7654f30baf613347bc384f16f4e9b817a374603d
lebrancconvas <lebrancconvas@gmail.com>
2022-03-16 16:18:31 +0700

    🎉 Init Squid Game Function.

---
+++ src/playground/SquidGame/Game/RedLightGreenLight/RedLightGreenLight.ts
@@ -0,0 +1,15 @@
+// Player 
+// Move while System songs. 
+// Stop when System stop to song. 
+
+// System 
+// Song
+// If stop song, Check player movement. 
+// If player move, Penalty. 
+// If player doesn't move, Skip. 
+
+interface Music {
+	songname: String;
+	duration: number;
+}
+
